d9910bdf66fcd0834d76a848bef3f8fcf5a862a8
ArthurSerenne <79085879+ArthurSerenne@users.noreply.github.com>
2021-05-31 15:40:22 +0200

    page connexion

--- Projet_Stage/index.php
+++ Projet_Stage/index.php
@@ -1,11 +1,32 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
 	<title>Projet_Stage</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/indexStyle.css">
 </head>
-<body>
-	salut
+	<body data-spy="scroll" data-target=".navbar" data-offset="50">
+		
+		<!-- On appelle la navbar -->
+		<?php
+		require('navbar.php');
+		?>
 
-</body>
+		<!-- Formulaire de connexion -->
+		<div class="loginbox">
+			<form action="" method="post">
+				<p>Nom d'utilisateur</p>
+				<input type="text" name="pseudo" placeholder="Entrez votre nom d'utilisateur">
+				<p>mot de passe</p>
+				<input type="password" name="password" placeholder="Entrez votre mot de passe">
+				<input type="submit" name="submit" value="valider">
+			</form>
+		</div>
+
+	</body>
 </html>
---
+++ Projet_Stage/navbar.php
@@ -0,0 +1,11 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="css/navbarStyle.css">
+</head>
+
+<section id="navbar">
+	<nav class="navbar navbar-dark bg-link">
+		<a class="navbar-brand" href="#">
+			<img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="">
+		</a>
+	</nav>
+</section>
