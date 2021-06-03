827ac508370c35b2a363b7e5b9178e24527c8320
N0SAFE <77668394+N0SAFE@users.noreply.github.com>
2021-06-03 13:40:58 +0200

    Add files via upload

--- Projet_Stage/script.js
+++ Projet_Stage/script.js
@@ -1,54 +1,44 @@
-window.onload = function () {
+var x = 0;
 
-    var chart = new CanvasJS.Chart("histogramme", {
-        animationEnabled: true,
-        exportEnabled: true,
-        theme: "light1",
-        title:{
-            text: "Mots de Passe les plus utilisés"
-        },
-        data: [{
-            type: "column",
-            indexLabelFontColor: "#5A5757",
-            indexLabelPlacement: "outside",
-            dataPoints: [
-                { x: 1, y: 71, indexLabel: "Highest"},
-                { x: 2, y: 55, indexLabel: "Highest" },
-                { x: 3, y: 50, indexLabel: "Highest"},
-                { x: 4, y: 65, indexLabel: "Highest"},
-                { x: 5, y: 92, indexLabel: "Highest"},
-                { x: 6, y: 68, indexLabel: "Highest"},
-                { x: 7, y: 38, indexLabel: "Highest"},
-                { x: 8, y: 71, indexLabel: "Highest"},
-                { x: 9, y: 54, indexLabel: "Highest"},
-                { x: 10, y: 60, indexLabel: "Highest"}
-            ]
-        }]
-    });
-    chart.render();
+function appendToChild(id, pointer) {
+	x++;
+	document.getElementById("toDelete").remove();
+	var element = document.getElementById(id);
+	var pointer = document.getElementById(pointer);
 
-    var chart = new CanvasJS.Chart("pie", {
-        theme: "light2",
-        exportEnabled: true,
-        animationEnabled: true,
-        title: {
-            text: "Moyenne Caractères Dans Les Mots De Passe"
-        },
-        data: [{
-            type: "pie",
-            startAngle: 25,
-            toolTipContent: "<b>{label}</b>: {y}%",
-            showInLegend: "true",
-            legendText: "{label}",
-            indexLabelFontSize: 16,
-            indexLabel: "{label} - {y}%",
-            dataPoints: [
-                { y: averageCharPass, label: "Alphabets" },
-                { y: averageNumPass, label: "Chiffres" },
-                { y: averageSpePass, label: "Caractères Spéciaux" }
-            ]
-        }]
-    });
-    chart.render();
+	var tag = document.createElement("tr");
+	tag.setAttribute("id", "tr");
+	element.appendChild(tag);
+	var tr = document.getElementById("tr");
 
+	var tag = document.createElement("td");
+	tag.setAttribute("id", "td");
+	tr.appendChild(tag);
+	var td = document.getElementById("td");
+	td.appendChild(document.createTextNode(x+1+"."));
+	td.setAttribute("id", "")
+
+	for (i = 0; i < 2; i++) {
+		var tag = document.createElement("td");
+		tag.setAttribute("id", "td");
+		tr.appendChild(tag);
+		var td = document.getElementById("td");
+		var tag = document.createElement("input");
+		tag.setAttribute("required", "")
+		tag.setAttribute("type", "radio");
+		tag.setAttribute("value", i);
+		tag.setAttribute("name", x);
+		td.appendChild(tag);
+		td.setAttribute("id", "")
+	}
+	tr.setAttribute("id", "");
+
+	var tag = document.createElement("button");
+	tag.setAttribute("type", "button")
+	tag.setAttribute("onclick", "appendToChild('point', 'buttonPointer')")
+	tag.setAttribute("id", "toDelete");
+	tag.setAttribute("class", "btn btn-primary");
+	var text = document.createTextNode("+");
+	tag.appendChild(text);
+	pointer.appendChild(tag);
 }
